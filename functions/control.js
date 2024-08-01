cfa10b17dab42ad48fa50b9d00ef2fe3c317ad3d
aymanrgab <aymanrar2c@gmail.com>
2024-08-01 15:04:36 +0300

    update

--- functions/control.js
+++ functions/control.js
@@ -1,11 +1,22 @@
 const faunadb = require('faunadb')
 const q = faunadb.query
-
-const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SECRET
-})
+const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET })
 
 exports.handler = async function(event, context) {
+  const headers = {
+    'Access-Control-Allow-Origin': 'chrome-extension://cghfdfnnfndgopbgmphmljjjbegnkeep',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: headers,
+      body: ''
+    };
+  }
+
   const extensionId = event.queryStringParameters.extensionId
   
   try {
@@ -15,11 +26,13 @@ exports.handler = async function(event, context) {
     
     return {
       statusCode: 200,
+      headers: headers,
       body: JSON.stringify({ action: response.data.action })
     }
   } catch (error) {
     return {
       statusCode: 400,
+      headers: headers,
       body: JSON.stringify({ error: 'Failed to retrieve control action' })
     }
   }
--- functions/stats.js
+++ functions/stats.js
@@ -1,13 +1,28 @@
 const faunadb = require('faunadb')
 const q = faunadb.query
-
-const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SECRET
-})
+const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET })
 
 exports.handler = async function(event, context) {
+  const headers = {
+    'Access-Control-Allow-Origin': 'chrome-extension://cghfdfnnfndgopbgmphmljjjbegnkeep',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: headers,
+      body: ''
+    };
+  }
+
   if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' }
+    return { 
+      statusCode: 405, 
+      headers: headers,
+      body: 'Method Not Allowed' 
+    }
   }
 
   const { extensionId, isRunning, lastActive } = JSON.parse(event.body)
@@ -24,11 +39,13 @@ exports.handler = async function(event, context) {
 
     return {
       statusCode: 200,
+      headers: headers,
       body: JSON.stringify({ message: 'Stats recorded successfully' })
     }
   } catch (error) {
     return {
       statusCode: 400,
+      headers: headers,
       body: JSON.stringify({ error: 'Failed to record stats' })
     }
   }
